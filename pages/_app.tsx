9a2f0e234686a9deeb02bc59b5e08e3af874678c
Saul Carlin <saul.carlin@gmail.com>
2022-06-24 13:53:24 -0700

    re-enable ssr

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import dynamic from 'next/dynamic'
-
-const AppWithoutSSR = dynamic(() => import('../components/App'), {
-  ssr: false,
-})
 
 function AppWrapper({ Component, pageProps }: AppProps) {
-  return (
-    <AppWithoutSSR>
-      <Component {...pageProps} />
-    </AppWithoutSSR>
-  )
+  return <Component {...pageProps} />
 }
 
 export default AppWrapper
